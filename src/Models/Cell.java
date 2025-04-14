b84856c924db769a0b827ff27bed2a18fffd2ba4
BrenoBrDev <brenobr.dev@gmail.com>
2025-04-14 14:57:02 -0500

    Update logic to display cell to make grid scalable and more readable.

--- src/Models/Cell.java
+++ src/Models/Cell.java
@@ -2,8 +2,15 @@ package Models;
 
 import Enumerations.CellType;
 import Interfaces.Displayable;
+import Utilities.Constants;
 
 public class Cell implements Displayable {
+    final String PREFIX = "  ";
+    final boolean IS_HORIZONTAL_HEADER;
+    final boolean IS_VERTICAL_HEADER;
+    final boolean IS_AFTER_COLUMN_9;
+    final boolean IS_LAST_COLUMN;
+
     private final Vector2 position;
 
     private CellType type = CellType.EMPTY;
@@ -11,28 +18,33 @@ public class Cell implements Displayable {
 
     public Cell(int x, int y) {
         this.position = new Vector2(x, y);
+        IS_HORIZONTAL_HEADER = position.getX() == 0;
+        IS_VERTICAL_HEADER = position.getY() == 0;
+        IS_AFTER_COLUMN_9 = position.getY() > 9;
+        IS_LAST_COLUMN = position.getY() == Constants.GRID_AREA - 1;
     }
 
     @Override
     public void display() {
-        String suffix;
+        String suffix = PREFIX;
 
-        if (position.getY() == 10)
-            suffix = "\n";
-        else
+        if (IS_AFTER_COLUMN_9 && IS_HORIZONTAL_HEADER)
             suffix = " ";
 
+        if (IS_LAST_COLUMN)
+            suffix = "\n";
+
         if (!isRevealed) {
-            System.out.print("." + suffix);
+            System.out.print(PREFIX + "-" + suffix);
             return;
         }
 
-        if (position.getX() == 0) {
-            System.out.print(position.getY() + suffix);
-        } else if (position.getY() == 0) {
-            System.out.print(Character.toString(position.getX() + 64) + suffix);
+        if (IS_HORIZONTAL_HEADER) {
+            System.out.print(PREFIX + position.getY() + suffix);
+        } else if (IS_VERTICAL_HEADER) {
+            System.out.print(PREFIX + Character.toString(position.getX() + 64) + suffix);
         } else {
-            System.out.print(type.toString() + suffix);
+            System.out.print(PREFIX + type.toString() + suffix);
         }
     }
 
--- src/Utilities/Constants.java
+++ src/Utilities/Constants.java
@@ -1,7 +1,7 @@
 package Utilities;
 
 public class Constants {
-    public static final int GRID_AREA = 14;
+    public static final int GRID_AREA = 11;
     public static final int MIN_SHIP_LENGTH = 2;
     public static final int MAX_SHIP_LENGTH = 5
 
